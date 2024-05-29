9b0c829e0e9a1404bb951831b651232470a39087
iloveyouu <157247059+JLoVeYouu@users.noreply.github.com>
2024-05-29 20:35:25 +0000

    'add very much new js code'

--- index.js
+++ index.js
@@ -32,3 +32,50 @@ fatherBlock.addEventListener('mouseout', function (e) {
         thirdImg.style.cssText = 'transform: scale(1); opacity: 1;'
     }
 })
+
+
+// scroll to .section__for-img //
+const jumpToSectionForImgButton = document.querySelector('.block-full-btn-bl')
+const sectionForImg = document.querySelector('.section__for-img')
+jumpToSectionForImgButton.onclick = () => {
+    sectionForImg.scrollIntoView(true)
+}
+
+// scroll to .section__more-h2-underline //
+const jumpToSectionmoreH2Underline = document.querySelector('.block-text-links:nth-of-type(2)')
+const sectionMoreH2Underline = document.querySelector('.section__more-h2-underline')
+jumpToSectionmoreH2Underline.addEventListener('click', (e) => {
+    e.preventDefault()
+    sectionMoreH2Underline.scrollIntoView(true)
+})
+
+const jumpToSectionmoreH2UnderlineAnother = document.querySelector('.simply-links-items:nth-of-type(5)')
+jumpToSectionmoreH2UnderlineAnother.onclick = () => {
+    sectionMoreH2Underline.scrollIntoView(true)
+}
+
+// scroll to .section__container-black-chief // 
+const jumpToSectionContainerBlackChief = document.querySelector('.block-text-links:last-child')
+const sectionContainerBlackChief = document.querySelector('.section__container-black-chief')
+jumpToSectionContainerBlackChief.addEventListener('click', (e) => {
+    e.preventDefault()
+    const blackChiefSectionTopOffset = sectionContainerBlackChief.getBoundingClientRect().top + window.scrollY
+    window.scrollTo(0, blackChiefSectionTopOffset)
+})
+
+// scroll to top page // 
+
+const jumpToTopPage = document.querySelector('.section-half-fot-text-with-button-button')
+jumpToTopPage.onclick = () => {
+    window.scrollTo(0, 0)
+}
+
+const jumpToTopPageAnother = document.querySelector('.section__name-latest-articles-with-sections-section-who-have-img-title-text-and-btn')
+jumpToTopPageAnother.addEventListener('click', (e) => {
+    let eTarget = event.target.classList
+    if (eTarget.contains('equivalent-section-who-have-btn-button')) {
+        window.scrollTo(0, 0)
+    }
+})
+
+
